44f0bdaa127adf77305c678aad1803f62ab84cd6
Ukeje Patience Ndidiamaka <patienceukeje3006@gmail.com>
2022-06-15 14:00:12 +0100

    pushing order page

--- buypower.js
+++ buypower.js
@@ -1,36 +1,7 @@
-nav a {
-    /*color: #fff;
-    font-size: 14px;*/
-    font-weight: 300;
-    text-transform: capitalize;
-    letter-spacing: 0.5px;
-    padding: 3px 10px;
-    border-bottom-left-radius: 10px;
-    border-top-right-radius: 10px;
-    -webkit-transition: 0.2s ease-in-out;
-    transition: 0.2s ease-in-out;
-}
-nav a {
-    color: #000;
-    font-size: 10px;
-}
-.button, button {
-    box-sizing: border-box;
-    color: #fff;
-    text-align: center;
-    /* padding: 25px; */
-    font-family: Open Sans;
-    font-weight: 600;
-    border-radius: 3px;
-    border: 1px solid #8ca23b;
-    background: linear-gradient(180deg,#b1c067 0,#8ca23b);
-    -webkit-appearance: button;
-    cursor: pointer;
-    line-height: normal;
-    vertical-align: baseline;
-    display: inline-block;
-    align-items: flex-start;
-}
-button {
-    padding: 25px;
+function fun() {
+    if (document.getElementById('disco').value!="") {
+        document.getElementById('buy-electricity-btn').disabled=false;
+    } else {
+        document.getElementById('buy-electricity-btn').disabled=true;
+    }
 }
